98b5655331b4265944ec3c8e0bfe2a28e2142c9a
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2026-01-28 21:49:17 -0300

    🔨 use opp to improve solution by implementing Triangle class

--- 05-oop/src/application/Program.java
+++ 05-oop/src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import entities.Triangle;
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -14,24 +16,26 @@ public class Program {
 
         Scanner sc = new Scanner(System.in);
         Locale.setDefault(Locale.US);
-        double xA, xB, xC;
-        double yA, yB, yC;
 
-        System.out.print("Enter the measures of triangle X: ");
-        xA = sc.nextDouble();
-        xB = sc.nextDouble();
-        xC = sc.nextDouble();
+        Triangle xTriangle, yTriangle;
+        xTriangle = new Triangle();
+        yTriangle = new Triangle();
 
-        System.out.print("Enter the measures of triangle Y: ");
-        yA = sc.nextDouble();
-        yB = sc.nextDouble();
-        yC = sc.nextDouble();
+        System.out.println("Enter the measures of triangle X: ");
+        xTriangle.a = sc.nextDouble();
+        xTriangle.b = sc.nextDouble();
+        xTriangle.c = sc.nextDouble();
 
-        double pX = (xA + xB + xC) / 2;
-        double pY = (yA + yB + yC) / 2;
+        System.out.println("Enter the measures of triangle Y: ");
+        yTriangle.a = sc.nextDouble();
+        yTriangle.b = sc.nextDouble();
+        yTriangle.c = sc.nextDouble();
 
-        double areaX = Math.sqrt(pX * (pX - xA) * (pX - xB) * (pX - xC));
-        double areaY = Math.sqrt(pY * (pY - yA) * (pY - yB) * (pY - yC));
+        double pX = (xTriangle.a + xTriangle.b + xTriangle.c) / 2;
+        double pY = (yTriangle.a + yTriangle.b + yTriangle.c) / 2;
+
+        double areaX = Math.sqrt(pX * (pX - xTriangle.a) * (pX - xTriangle.b) * (pX - xTriangle.c));
+        double areaY = Math.sqrt(pY * (pY - yTriangle.a) * (pY - yTriangle.b) * (pY - yTriangle.c));
 
         System.out.printf("Triangle X area: %.4f\n", areaX);
         System.out.printf("Triangle Y area: %.4f\n", areaY);
